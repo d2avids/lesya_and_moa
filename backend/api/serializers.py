af5de19d976f84556da688832e363ac85db41d7a
David Saidov <delightxxls@gmail.com>
2024-08-09 21:12:52 +0300

    refactor app names

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from news.models import News
 from tasks.models import Task
 from users.models import Region
 
@@ -14,3 +15,15 @@ class RegionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Region
         fields = ('id', 'name',)
+
+
+class ShortNewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ('id', 'title', 'description', 'image', 'created_at')
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ('id', 'title', ' description', 'image', 'text', 'created_at')
