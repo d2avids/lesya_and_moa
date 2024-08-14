69bd8b9ba84bf595240c48821a24b9b1d2e17005
David Saidov <delightxxls@gmail.com>
2024-08-14 18:31:39 +0300

    improve pagination (added total key)

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,20 @@
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
 class Limit100OffsetPagination(LimitOffsetPagination):
     max_limit = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.total_count = queryset.count()
+
+        return super().paginate_queryset(queryset, request, view)
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total': self.total_count,
+            'count': len(data),
+            'limit': self.limit,
+            'offset': self.offset,
+            'results': data
+        })
